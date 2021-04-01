08d0cb69a81081f4d48bd9dcb707c127b694291a
chica25 <patricia@patriciaco.com>
2021-04-01 01:45:08 -0400

    Modify function

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const size = images[0].clientWidth;
 slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click', () => {
+   if(counter >= images.length -1) return;
     slide.style.transition = 'tranform 0.4s ease-in-out';
     counter++;
     slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
@@ -17,7 +18,21 @@ nextBtn.addEventListener('click', () => {
 })
 
 prevBtn.addEventListener('click', () => {
+    if(counter <= 0) return;
     slide.style.transition = 'tranform 0.4s ease-in-out';
     counter--;
     slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+slide.addEventListener('transitioned', () => {
+  if(images[counter].id === 'last-img-copy'){
+      slide.style.transform = 'none';
+      counter = images.length - 2;
+      slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(images[counter].id === 'last-img-copy'){
+        slide.style.transform = 'none';
+        counter = images.length - counter;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
 })
